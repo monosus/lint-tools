d4a75993fc6ea7a9e66c925800a9109ea91a8df1
monoharada <harada@monosus.co.jp>
2024-07-04 16:23:35 +0900

    fix: import writeFileSync

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import { readFileSync, existsSync, renameSync } from 'node:fs';
-import { exec } from 'node:child_process';
-import readline from 'node:readline';
+import { readFileSync, existsSync, renameSync, writeFileSync } from "node:fs";
+import { exec } from "node:child_process";
+import readline from "node:readline";
 
 // package.jsonファイルを読み込む
-const packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
 
 // devDependenciesのパッケージ名を取得
 const devDependencies = Object.keys(packageJson.devDependencies);
 
 // コマンドを作成
-const command = `bun add --dev --cwd ../ ${devDependencies.join(' ')}`;
+const command = `bun add --dev --cwd ../ ${devDependencies.join(" ")}`;
 
 // ユーザーに確認を求める
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 function askQuestion(query) {
@@ -22,8 +22,10 @@ function askQuestion(query) {
 }
 
 async function main() {
-  const answer = await askQuestion(`次のコマンドを実行しますか？ (yes/no): ${command}\n`);
-  if (answer.toLowerCase() === 'yes' || answer.trim() === '') {
+  const answer = await askQuestion(
+    `次のコマンドを実行しますか？ (yes/no): ${command}\n`
+  );
+  if (answer.toLowerCase() === "yes" || answer.trim() === "") {
     try {
       await execCommand(command);
       await handleEditorConfig();
@@ -35,7 +37,7 @@ async function main() {
       rl.close();
     }
   } else {
-    console.log('コマンドの実行をキャンセルしました。');
+    console.log("コマンドの実行をキャンセルしました。");
     rl.close();
   }
 }
@@ -57,51 +59,59 @@ function execCommand(command) {
 }
 
 async function handleEditorConfig() {
-  const editorConfigPath = './.editorconfig';
+  const editorConfigPath = "./.editorconfig";
   if (existsSync(editorConfigPath)) {
-    const answer = await askQuestion('.editorconfigファイルを親階層に移動しますか？ (yes/no): ');
-    if (answer.toLowerCase() === 'yes' || answer.trim() === '') {
-      renameSync(editorConfigPath, '../.editorconfig');
-      console.log('.editorconfigファイルを親階層に移動しました。');
+    const answer = await askQuestion(
+      ".editorconfigファイルを親階層に移動しますか？ (yes/no): "
+    );
+    if (answer.toLowerCase() === "yes" || answer.trim() === "") {
+      renameSync(editorConfigPath, "../.editorconfig");
+      console.log(".editorconfigファイルを親階層に移動しました。");
     } else {
-      console.log('.editorconfigファイルの移動をキャンセルしました。');
+      console.log(".editorconfigファイルの移動をキャンセルしました。");
     }
   } else {
-    console.log('.editorconfigファイルがルート直下に存在しません。');
+    console.log(".editorconfigファイルがルート直下に存在しません。");
   }
 }
 
 async function handleLeftHook() {
-  const answer = await askQuestion('left-hookを導入しますか？ (yes/no): ');
-  if (answer.toLowerCase() === 'yes' || answer.trim() === '') {
+  const answer = await askQuestion("left-hookを導入しますか？ (yes/no): ");
+  if (answer.toLowerCase() === "yes" || answer.trim() === "") {
     try {
-      const leftHookConfigPath = './lefthook.yml';
+      const leftHookConfigPath = "./lefthook.yml";
       if (existsSync(leftHookConfigPath)) {
-        await renameSync(leftHookConfigPath, '../lefthook.yml');
-        console.log('lefthook.ymlファイルを親階層に移動しました。');
-        await execCommand('bun add --dev --cwd ../ lefthook');
-        await execCommand('cd .. && bunx lefthook install');
+        await renameSync(leftHookConfigPath, "../lefthook.yml");
+        console.log("lefthook.ymlファイルを親階層に移動しました。");
+        await execCommand("bun add --dev --cwd ../ lefthook");
+        await execCommand("cd .. && bunx lefthook install");
       } else {
-        console.log('lefthook.ymlファイルがルート直下に存在しません。');
+        console.log("lefthook.ymlファイルがルート直下に存在しません。");
       }
     } catch (error) {
       console.error(`エラー: ${error.message}`);
     }
   } else {
-    console.log('lefthookの導入をキャンセルしました。');
+    console.log("lefthookの導入をキャンセルしました。");
   }
 }
 
 async function addLintScript() {
-  const parentPackageJsonPath = '../package.json';
+  const parentPackageJsonPath = "../package.json";
   if (existsSync(parentPackageJsonPath)) {
-    const parentPackageJson = JSON.parse(readFileSync(parentPackageJsonPath, 'utf8'));
+    const parentPackageJson = JSON.parse(
+      readFileSync(parentPackageJsonPath, "utf8")
+    );
     parentPackageJson.scripts = parentPackageJson.scripts || {};
     parentPackageJson.scripts.lint = "bun ./lint-tools/linting.ts";
-    writeFileSync(parentPackageJsonPath, JSON.stringify(parentPackageJson, null, 2), 'utf8');
-    console.log('親階層のpackage.jsonにlintスクリプトを追加しました。');
+    writeFileSync(
+      parentPackageJsonPath,
+      JSON.stringify(parentPackageJson, null, 2),
+      "utf8"
+    );
+    console.log("親階層のpackage.jsonにlintスクリプトを追加しました。");
   } else {
-    console.log('親階層にpackage.jsonが存在しません。');
+    console.log("親階層にpackage.jsonが存在しません。");
   }
 }
 
